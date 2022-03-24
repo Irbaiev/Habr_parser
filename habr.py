7b46282d2f66bc74dc6f11442941fc069c393350
irbaiev <a.irbaeiv75@gmail.com>
2022-03-24 13:23:16 +0300

    error psql

--- habr.py
+++ habr.py
@@ -16,23 +16,31 @@ headers = [
         'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8'}
 ]
 
-
+posts = []
 
 def habr_parsing():
 
-    url = 'https://habr.com/ru/all/page1/'
+    url = 'https://habr.com/ru/all/'
     res = requests.get(url, headers=choice(headers))
-    posts =[]
-
-    if res.status_code == 200:
+    pagination = len(BS(res.content, 'html.parser').find(
+        'div', class_='tm-pagination').find_all('a'))
+    for u in range(pagination):
+        url = f'https://habr.com/ru/all/{u}/'
+        if res.status_code == 200:
             soup = BS(res.content, 'html.parser')
-            article = soup.find('article', class_ = 'tm-articles-list__item').find_all_next('div', class_ = 'tm-article-snippet')
-            for i in article:
+            article =soup.find('article',class_='tm-articles-list__item')
+            div = article.find_all_next('div',class_='tm-article-snippet')
+            for i in div:
+                title = i.find('h2',class_='tm-article-snippet__title tm-article-snippet__title_h2').text.strip()
                 user = i.find('a', class_ = 'tm-user-info__username').text.strip()
-                profile = 'https://habr.com' + i.a['href']
                 url = 'https://habr.com' + i.find('h2', class_ = 'tm-article-snippet__title tm-article-snippet__title_h2').a['href']
-                title = i.find('h2', class_ = 'tm-article-snippet__title tm-article-snippet__title_h2').text.strip()
-                posts.append((user, profile, url, title))
+                profile = 'https://habr.com' + i.a['href']
+                # image = i.find('img', class_='tm-article-snippet__lead-image').a['src']
+                description = i.find('div', class_='article-formatted-body article-formatted-body_version-2')
+                views = i.find('span', class_='tm-icon-counter tm-data-icons__item')
+                comm = i.find('div',class_='tm-article-comments-counter-link tm-data-icons__item')
+                posts.append({'title':title,'user':user,  'url':url,'profile':profile,
+                            'views':views,'comm':comm,'description':description})
     return posts
     # conn = sqlite3.connect('posts.db')
 
--- send_email.py
+++ send_email.py
@@ -18,14 +18,14 @@ from habr import habr_parsing
 
 posts = habr_parsing()
 emails = Follower.objects.all()
-subject = 'Посты на сегодня {datetime.now()}'
+subject = 'Посты на сегодня {datetime.today()}'
 from_email = EMAIL_HOST_USER
 text_content = 'Посты на сегодня'
 html_content = ''
 for email in emails:
     for post in posts:
-        html_content += f'''<a href = "{ post.get('profile') }"<h5>{ post.get('user') }</h5></a> 
-              <h3 class="card-title">{ post.get('title') }</h3> 
+        html_content += f'''<a href = "{ post.get('profile') }"<h5>{ post.get('user') }</h5></a> <br>
+              <h3 class="card-title">{ post.get('title') }</h3> <br>
               <a href="{ post.get('urls') }" class="btn btn-primary">Читать далее</a><br>'''
     msg = EmailMultiAlternatives(subject, text_content, from_email, [email])
     msg.attach_alternative(html_content, 'text/html')
